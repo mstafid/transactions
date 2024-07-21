dc1091a35b0deada462a0d116da19a0d02f1d507
mstafid <amnamustafidfaiz@gmail.com>
2024-07-22 00:34:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
-import joblib
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 # Memuat model terbaik
 model = joblib.load('best_model.pkl')
@@ -10,7 +10,7 @@ model = joblib.load('best_model.pkl')
 data = pd.read_csv('transactions.csv')
 
 # Daftar kolom yang diperlukan selama pelatihan
-required_columns = ['Sender UPI ID', 'Receiver UPI ID', 'Amount (INR)']
+required_columns = ['Sender Name', 'Receiver Name', 'Amount (INR)', 'status']
 
 # Pastikan hanya kolom yang diperlukan ada
 data = data[required_columns]
@@ -42,8 +42,7 @@ def preprocess_input(user_input):
     processed_input[numeric_features] = scaler.transform(processed_input[numeric_features])
     return processed_input
 
-# Antarmuka Streamlit
-st.title("Prediksi Status Transaksi")
+st.title('Prediksi Status Transaksi')
 
 st.markdown("""
     <style>
@@ -51,30 +50,31 @@ st.markdown("""
         background-color: #f0f0f5;
     }
     </style>
-    <h3>Masukkan Data Transaksi</h3>
+    <h3>Masukkan Data Pelanggan</h3>
 """, unsafe_allow_html=True)
 
-# Input pengguna
-sender_upi_id = st.text_input('Sender UPI ID')
-receiver_upi_id = st.text_input('Receiver UPI ID')
-amount_inr = st.number_input('Amount (INR)', min_value=0.0, format="%f")
+# Input user
+sender_name = st.text_input('Nama Pengirim')
+receiver_name = st.text_input('Nama Penerima')
+amount = st.number_input('Jumlah (INR)', min_value=0)
+status = st.selectbox('Status', ['Sukses', 'Gagal'])
 
 user_input = {
-    'Sender UPI ID': sender_upi_id,
-    'Receiver UPI ID': receiver_upi_id,
-    'Amount (INR)': amount_inr
+    'Sender Name': sender_name,
+    'Receiver Name': receiver_name,
+    'Amount (INR)': amount,
+    'Status': status
 }
 
 if st.button('Predict'):
     user_input_processed = preprocess_input(user_input)
     try:
         prediction = model.predict(user_input_processed)
-        status = "Approved" if prediction[0] == 1 else "Rejected"
-        st.write(f'Prediction: {status}')
+        st.write(f'Prediction: {prediction[0]}')
     except ValueError as e:
         st.error(f"Error in prediction: {e}")
 
-# Tambahkan elemen HTML untuk output
+ # Tambahkan elemen HTML untuk output
 st.markdown("""
     <h3>Output Prediksi</h3>
     <p>Hasil prediksi akan ditampilkan di sini.</p>
